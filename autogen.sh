97f7680320d872facc31822ee8617b76b1b5c85b
Jansen Garrick <janseng@protonmail.com>
2018-01-01 21:38:40 +0100

    clean submodules before update/init

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
+pushd tor && git checkout -f . && popd
+pushd leveldb && git checkout -f . && popd
 git submodule update --init
 
 autoreconf --no-recursive --install
 
 pushd tor
-git checkout -f .
 patch --no-backup-if-mismatch -f -p0 < ../tor-or-am.patch
 patch --no-backup-if-mismatch -f -p0 < ../tor-am.patch
 ./autogen.sh
 popd
 
 pushd leveldb
-git checkout -f .
 patch --no-backup-if-mismatch -f -p1 < ../leveldb.patch
 popd
