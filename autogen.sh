dc60001bc339655b1fe72b3386a773579f56908e
HLXEasy <hlxeasy@gmail.com>
2018-07-28 22:24:55 +0200

    Added real checkout of proper submodule branch

--- autogen.sh
+++ autogen.sh
@@ -7,6 +7,13 @@ git submodule init
 git submodule sync --recursive
 git submodule update --recursive --force --remote
 
+branchToUse=cmake-migration
+for submodule in db4.8 leveldb tor ; do
+    cd ${submodule}
+    git checkout ${branchToUse}
+    cd -
+done
+
 # Disabled as we are using fully configured repos now!
 #autoreconf --no-recursive --install
 #
