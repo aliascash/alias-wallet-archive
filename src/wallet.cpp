9e4d385c7fbb53f9ce04992fb55a77577ccfa444
teknex <teknex@tekener.net>
2018-08-19 00:05:53 +0200

    #75 fix fee calculation if amount+fee exceeds balance #6 introduce feeMode for PickAnonInputs()

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -3994,11 +3994,11 @@ static bool checkCombinations(int64_t nReq, int m, std::vector<COwnedAnonOutput*
     return false;
 }
 
-int CWallet::PickAnonInputs(int rsType, int64_t nValue, int64_t& nFee, int nRingSize, CWalletTx& wtxNew, int nOutputs, int nSizeOutputs, int& nExpectChangeOuts, std::list<COwnedAnonOutput>& lAvailableCoins, std::vector<COwnedAnonOutput*>& vPickedCoins, std::vector<std::pair<CScript, int64_t> >& vecChange, bool fTest, std::string& sError)
+int CWallet::PickAnonInputs(int rsType, int64_t nValue, int64_t& nFee, int nRingSize, CWalletTx& wtxNew, int nOutputs, int nSizeOutputs, int& nExpectChangeOuts, std::list<COwnedAnonOutput>& lAvailableCoins, std::vector<COwnedAnonOutput*>& vPickedCoins, std::vector<std::pair<CScript, int64_t> >& vecChange, bool fTest, std::string& sError, int feeMode)
 {
     if (fDebugRingSig)
-        LogPrintf("PickAnonInputs(), ChangeOuts %d\n", nExpectChangeOuts);
-    // - choose the smallest coin that can cover the amount + fee
+        LogPrintf("PickAnonInputs(), ChangeOuts %d, FeeMode %d\n", nExpectChangeOuts, feeMode);
+    // - choose the smallest coin that can cover the amount (feeMode==1) or the amount + fee (feeMode!=1)
     //   or least no. of smallest coins
 
 
@@ -4054,20 +4054,47 @@ int CWallet::PickAnonInputs(int rsType, int64_t nValue, int64_t& nFee, int nRing
 
         nFee = wtxNew.GetMinFee(0, GMF_ANON, nTotalBytes);
 
-        if (fDebugRingSig)
-            LogPrintf("nValue + nFee: %d, nValue: %d, nAmountCheck: %d, nTotalBytes: %u\n", nValue + nFee, nValue, nAmountCheck, nTotalBytes);
+		int64_t nValueTest;		
+		if (feeMode == 1) {
+			nValueTest = nValue;
+		}
+		else {
+			nValueTest = nValue + nFee;
+			
+			int nFeeDiff = nAmountCheck - nValueTest;
+			if (nFeeDiff < 0)
+			{
+				// substract fee
+				nValueTest += nFeeDiff;
+				if (fDebugRingSig)
+					LogPrintf("AmountWithFeeExceedsBalance! simulate exhaustive trx, lower amount by nFeeDiff: %d\n", nFeeDiff);
 
-        if (nValue + nFee > nAmountCheck)
-        {
-            sError = "Not enough mature coins with requested ring size.";
-            return 3;
-        };
+				if (nExpectChangeOuts != 0) {
+					// -- set nExpectChangeOuts to 0 to simulate exhaustive payment (total+fee=totalAvailableCoins)
+					nExpectChangeOuts = 0;
+					// -- get nTotalBytes again for 0 change outputs
+					uint32_t nTotalBytes = (4 + 4 + 4) // Ctx: nVersion, nTime, nLockTime
+						+ GetSizeOfCompactSize(nOutputs)
+						+ nSizeOutputs
+						+ (GetSizeOfCompactSize(MIN_ANON_OUT_SIZE) + MIN_ANON_OUT_SIZE + sizeof(int64_t)) * vecChange.size()
+						+ GetSizeOfCompactSize((i + 1))
+						+ nByteSizePerInCoin * (i + 1);
+
+					if (fDebugRingSig)
+						LogPrintf("New nTotalBytes: %d\n", nTotalBytes);
+				}
+			}
+		}
+		
+		if (fDebugRingSig)
+			LogPrintf("nValue: %d, nFee: %d, nValueTest: %d, nAmountCheck: %d, nTotalBytes: %u\n", nValue, nFee, nValueTest, nAmountCheck, nTotalBytes);
 
         vPickedCoins.clear();
         vecChange.clear();
 
         std::vector<int> vecInputIndex;
-        if (checkCombinations(nValue + nFee, i+1, vData, vecInputIndex))
+
+        if (checkCombinations(nValueTest, i+1, vData, vecInputIndex))
         {
             if (fDebugRingSig)
             {
@@ -4087,7 +4114,7 @@ int CWallet::PickAnonInputs(int rsType, int64_t nValue, int64_t& nFee, int nRing
                 nTotalIn += vPickedCoins[ic]->nValue;
             };
 
-            int64_t nChange = nTotalIn - (nValue + nFee);
+            int64_t nChange = nTotalIn - (nValueTest + nFee);
 
 
             CStealthAddress sxChange;
@@ -4126,6 +4153,16 @@ int CWallet::PickAnonInputs(int rsType, int64_t nValue, int64_t& nFee, int nRing
             };
 
             nFee = nTestFee;
+
+			if (feeMode != 1 && nValue + nFee > nAmountCheck)
+			{
+				sError = "Not enough (mature) coins with requested ring size to cover amount with fees.";
+				if (fDebugRingSig)
+					LogPrintf("Not enough (mature) coins %d with requested ring size to cover amount %d together with fees %d.\n", nAmountCheck, nValue, nFee);
+				return 3;
+			};
+
+
             return 1; // found
         };
     };
