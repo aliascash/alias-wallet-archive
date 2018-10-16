b29bac447ea57694f75afe8e9c67bf06687f9f53
HLXEasy <hlxeasy@gmail.com>
2018-10-16 06:51:37 +0200

    Removed separate trigger of system shutdown as it is started right on system start itself

--- scripts/win-startShutdownTimer.bat
+++
@@ -1,4 +0,0 @@
-:: Script to start predefined shutdown using http://www.den4b.com/products/shutter
-
-:: Stored preset: Shutdown if CPU usage is lower than 10% for more than 60s
-"C:\Program Files (x86)\Shutter\Shutter.exe" /preset shutdownAfter60s
