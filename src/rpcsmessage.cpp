dbb7e2ead3386f13fadd8965aaf5a97e852426ce
teknex <teknex@gmail.com>
2018-10-17 21:13:33 +0200

    Add (c) for Spectrecoin developers

--- src/rpcsmessage.cpp
+++ src/rpcsmessage.cpp
@@ -1,4 +1,5 @@
-// Copyright (c) 2014 The ShadowCoin developers
+// Copyright (c) 2014-2016 The ShadowCoin developers
+// Copyright (c) 2016 The Spectrecoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
