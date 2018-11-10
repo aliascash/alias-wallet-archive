abb90a42e662209a3d613219fcac472fa2409372
HLXEasy <hlxeasy@gmail.com>
2018-11-11 00:23:26 +0100

    #117: Fixed Windows batch script

--- scripts/win-genbuild.bat
+++ scripts/win-genbuild.bat
@@ -1,21 +1,21 @@
 :: Helper script to create build.h.
-
+echo off
 set SRC_DIR=%cd%\src
 set CALL_DIR=%cd%
 
 cd
 cd %SRC_DIR%
-dir
 
 echo on
 
-echo "Creating build.h"
+@echo Creating build.h
 
-FOR /F %i in ('git describe --dirty') do set DESC=%i
-echo "#define BUILD_DESC \"%DESC%\"" > build.sh
+FOR /F "delims=" %%i IN ('git describe --dirty') DO set DESC=%%i
+@echo #define BUILD_DESC "%DESC%" > build.h
 
-FOR /F %i in ('git log -n 1 --format="%ci"') do set TIME=%i
-echo "#define BUILD_DATE \"%TIME%\"" >> build.sh
+FOR /F "delims=" %%j IN ('git log -n 1 --format^="%%ci"') DO set TIME=%%j
+@echo #define BUILD_DATE "%TIME%" >> build.h
 
-echo "Everything is OK"
+@echo Created build.h with the following content:
+type build.h
 cd %CALL_DIR%
