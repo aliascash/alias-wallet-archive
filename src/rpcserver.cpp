39bd62ec6afc38874255ef4ddd61292f2b36285b
teknex <teknex@gmail.com>
2019-04-22 00:10:33 +0200

    Integrate gettxout: cosmetic

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -312,7 +312,7 @@ static const CRPCCommand vRPCCommands[] =
     { "createmultisig",         &createmultisig,         true,      false,     true  },
     { "addredeemscript",        &addredeemscript,        false,     false,     false },
     { "getrawmempool",          &getrawmempool,          true,      false,     false },
-    {  "gettxout",              &gettxout,               true,      false,     false },
+    { "gettxout",               &gettxout,               false,     false,     false },
     { "getblock",               &getblock,               false,     false,     false },
     { "getblockbynumber",       &getblockbynumber,       false,     false,     false },
     { "setbestblockbyheight",   &setbestblockbyheight,   false,     false,     false },
