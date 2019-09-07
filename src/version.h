b5c73f70e53db4bd43988dd75c2558a6ade1af2a
teknex <teknex@gmail.com>
2019-09-07 10:34:24 +0200

    Increase protocol version to 60040 for V4. Disconnect older clients after fork.

--- src/version.h
+++ src/version.h
@@ -32,16 +32,16 @@ static const int DATABASE_VERSION = 70512;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60030;
+static const int PROTOCOL_VERSION = 60040;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 60020;
+static const int MIN_PEER_PROTO_VERSION = 60030;
 
 // Enforce minimum protocol version for chain V3.
-static const int LEGACY_CUTOFF_MIN_PROTOCOL_VERSION = 60030;
+static const int LEGACY_CUTOFF_MIN_PROTOCOL_VERSION = 60040;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
