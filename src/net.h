6d5063e800d102d712a2f50227fa5debed8b8a64
T0m14 <38764101+T0m14@users.noreply.github.com>
2019-09-29 19:14:26 +0200

    Tor Hidden Service v3 implementation + minor Tor improvements (#216)
    * Tor Hidden Service v3 implementation + minor Tor improvements
    
    * Changed bool variable name
    
    * Shorter operator for CNetAddr
    
    * Correctly flush addrman entries after peers.dat recreation
    
    * Force the use of the correct hostname when changing v2 <=> v3 addresses in Tor
    
    * Now verify if the file exists before checking its size
    
    * Fixed V3 addresses relaying, fixed addresses pollution on older nodes, and increased protocol version
    
    * Downgrade protocol version to 60041
    
    * Fixed archiving step on feature branch builds
    
    * Now, we will not try to send V3 addresses to older nodes
    
    * Ommit useless IsTorV3() check
    
    * Bump clientversion to 4.1.0.2
    
    * Tor Hidden Service v3 implementation + minor Tor improvements
    
    * Changed bool variable name
    
    * Shorter operator for CNetAddr
    
    * Correctly flush addrman entries after peers.dat recreation
    
    * Force the use of the correct hostname when changing v2 <=> v3 addresses in Tor
    
    * Now verify if the file exists before checking its size
    
    * Fixed V3 addresses relaying, fixed addresses pollution on older nodes, and increased protocol version
    
    * Downgrade protocol version to 60041
    
    * Now, we will not try to send V3 addresses to older nodes
    
    * Ommit useless IsTorV3() check
    
    * Bump clientversion to 4.1.0
    
    * SetupWalletWizard if no wallet.dat exists (Supports #82)
    
    * Add setupwalletwizard.cpp to Makefile.am
    
    * Smaller watermark image für SetupWalletWizard
    
    * Fixed archiving step on feature branch builds
    
    * Implement import wallet.dat
    
    * Bump clientversion to 4.1.0.1
    
    * Improve seed word verification UI
    
    * Remplace no longer working \n with <br>
    
    * Revised texts, Application icon
    
    * Always show first message on help
    
    * Show instructions on mnemonic creation page
    
    * fix NewMnemonicVerificationPage cleanup
    
    * Remove add/remove sub-wallet actions from UI
    
    * Remove key shortcut for mnemonic fields in verification and recover page
    
    * Remove null_addr in serialization
    
    * Bump clientversion to 4.1.0
    
    * Always prefer a v3 address to a v2 address
    
    * Add extra debug informations
    
    * We still use protocol 60041
    
    * Reduce network overhead by 70% when relaying v2 addresses, and by 28% when relaying v3 addresses. Increase protocol version to 60042
    
    * Add nVersion to debug infos
    
    * Add brackets to Clear() function
    
    * Add missing parenthesis
    
    * Factorizing some code
    
    * Be careful to always consistently initialize all fields
    
    * Make sure to memset ip field on error
    
    * Respect original behaviour not to wipe memory on error
    
    * Fixed bug on < operator
    
    * Remove bool fTorV3
    
    * Restore Toms Operator implementation
    
    * Make sure to wipe ip_tor memory when doing a SetSpecial() with a v2 address
    
    * Getting rid of fTorV3 boolean
    
    * Client version 4.1.0.3
    
    * Correct onion format use
    
    * More modulable address type handling
    
    * Revert debug informations to normal
    
    * Change default SocksPort for testnet
    
    * Move onion_port declaration
    
    * Remove const from onion_port variable
    
    * Remove duplicate SocksPort line

--- src/net.h
+++ src/net.h
@@ -469,7 +469,7 @@ public:
         // Known checking here is only to save space from duplicates.
         // SendMessages will filter it again for knowns that were added
         // after addresses were pushed.
-        if (addr.IsValid() && !setAddrKnown.count(addr))
+        if (addr.IsValid() && !setAddrKnown.count(addr) && (!addr.IsTorV3() || nRecvVersion >= 60042))
             vAddrToSend.push_back(addr);
     }
 
