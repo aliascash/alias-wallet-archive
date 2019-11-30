cbf77e83f2c83a6e87fded816afe81d8d3bae506
HLXEasy <hlxeasy@gmail.com>
2019-11-30 08:35:01 +0000

    Fixed autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-[[ -d .git ]] && [[ -d leveldb ]] && [[ -d extern/openssl-cmake ]] || \
+[[ -d .git ]] && [[ -d leveldb ]] && [[ -d external/openssl-cmake ]] || \
   { echo "Please run this command from the root of the Spectrecoin repository." && exit 1; }
 
 git submodule init
@@ -8,4 +8,4 @@ git submodule sync --recursive
 git submodule update --recursive --force --remote
 
 echo "Preparation finished."
-echo "To build, just run ./scripts/cmake-build.sh or ./scripts/cmake-build-android.sh"
+echo "To build, just run one of the ./scripts/cmake-build*.sh scripts"
