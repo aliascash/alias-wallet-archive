02de63ffd9c5867fdb654298ceb83e71cf06e380
HLXEasy <hlxeasy@gmail.com>
2020-03-15 19:11:23 +0000

    Use https:// to clone libevent

--- scripts/cmake-build-android_arm64.sh
+++ scripts/cmake-build-android_arm64.sh
@@ -198,7 +198,8 @@ checkEventLibClone(){
         git pull --prune
     else
         info "Cloning libevent"
-        git clone git@github.com:azat/libevent.git libevent
+#        git clone git@github.com:azat/libevent.git libevent
+        git clone https://github.com/azat/libevent.git libevent
     fi
     cd "${currentDir}"
 }
