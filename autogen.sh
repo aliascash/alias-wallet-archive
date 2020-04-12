b6581383d0a5c0b1143552b988f53438508fd46f
HLXEasy <hlxeasy@gmail.com>
2020-04-12 16:00:19 +0000

    Removed obsolete cmds on autogen script

--- autogen.sh
+++ autogen.sh
@@ -7,11 +7,5 @@ git submodule init
 git submodule sync --recursive
 git submodule update --recursive --force --remote
 
-# We need to get back to #4cb80b7 on leveldb
-# as HEAD is not building properly with CMake
-cd external/leveldb
-git checkout 4cb80b7
-cd - >/dev/null
-
 echo "Preparation finished."
 echo "To build, just run one of the ./scripts/cmake-build*.sh scripts"
