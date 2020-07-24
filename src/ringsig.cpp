0b7722df60dcf938b201e9c59e1218d613a32c26
Helix <hlxeasy@gmail.com>
2020-07-25 00:10:46 +0200

    Fixed license comments on src/r*

--- src/ringsig.cpp
+++ src/ringsig.cpp
@@ -1,7 +1,7 @@
-// Copyright (c) 2014-2016 The ShadowCoin developers
-// Copyright (c) 2016-2019 The Spectrecoin developers
-// Distributed under the MIT/X11 software license, see the accompanying
-// file license.txt or http://www.opensource.org/licenses/mit-license.php.
+// SPDX-FileCopyrightText: © 2016 SpectreCoin Developers
+// SPDX-FileCopyrightText: © 2014 ShadowCoin Developers
+//
+// SPDX-License-Identifier: MIT
 
 #include "ringsig.h"
 #include "base58.h"
