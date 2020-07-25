1a514be040a3eef8e7cb7cf3ee04b66ebfb35cb7
HLXEasy <hlxeasy@gmail.com>
2020-07-25 18:32:22 +0200

    Added license comments on unit tests

--- src/test/base32_tests.cpp
+++ src/test/base32_tests.cpp
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: © 2020 Alias Developers
+// SPDX-FileCopyrightText: © 2016 SpectreCoin Developers
+//
+// SPDX-License-Identifier: MIT
+
 #include <boost/test/unit_test.hpp>
 
 #include "util.h"
