bd9f2e3cd3cde8494bc2e3a8141337c411be35a9
Helix <hlxeasy@gmail.com>
2020-07-25 20:20:43 +0200

    Fixed license comments on src/json/*

--- src/json/json_spirit_writer.h
+++ src/json/json_spirit_writer.h
@@ -1,10 +1,11 @@
-#ifndef JSON_SPIRIT_WRITER
-#define JSON_SPIRIT_WRITER
-
-//          Copyright John W. Wilkinson 2007 - 2009.
-// Distributed under the MIT License, see accompanying file LICENSE.txt
+// SPDX-FileCopyrightText: © 2007 John W. Wilkinson
+//
+// SPDX-License-Identifier: MIT
 
 // json spirit version 4.03
+
+#ifndef JSON_SPIRIT_WRITER
+#define JSON_SPIRIT_WRITER
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1020)
 # pragma once
@@ -15,7 +16,7 @@
 
 namespace json_spirit
 {
-    // functions to convert JSON Values to text, 
+    // functions to convert JSON Values to text,
     // the "formatted" versions add whitespace to format the output nicely
 
     void         write          ( const Value& value, std::ostream&  os );
