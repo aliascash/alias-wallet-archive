66cbab84daadb3b8938af63de06a4b72fd663cf8
HLXEasy <hlxeasy@gmail.com>
2020-09-02 14:40:22 +0200

    Fixed deprecation errors for Qt 5.15 on Mac

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -249,7 +249,7 @@ void restoreWindowGeometry(const QString& strSetting, const QSize& defaultSize, 
     QSize size = settings.value(strSetting + "Size", defaultSize).toSize();
 
     if (!pos.x() && !pos.y()) {
-        QRect screen = QApplication::desktop()->screenGeometry();
+        QRect screen = QGuiApplication::primaryScreen()->geometry();
         pos.setX((screen.width() - size.width()) / 2);
         pos.setY((screen.height() - size.height()) / 2);
     }
