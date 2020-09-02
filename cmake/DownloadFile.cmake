510ce3933cf15fe49d1a52ca484f88141bd85eed
HLXEasy <hlxeasy@gmail.com>
2020-09-02 22:33:28 +0200

    Added helper functin to download files if required for build

---
+++ cmake/DownloadFile.cmake
@@ -0,0 +1,8 @@
+function(download_file url filename hash_type hash)
+    if (NOT EXISTS ${filename})
+        file(DOWNLOAD ${url} ${filename}
+                TIMEOUT 60  # seconds
+                EXPECTED_HASH ${hash_type}=${hash}
+                TLS_VERIFY ON)
+    endif ()
+endfunction(download_file)
