447dd0872913f5ff5f4a545aeefb4e77f19ecc53
HLXEasy <hlxeasy@gmail.com>
2020-09-03 09:36:01 +0200

    Added missing license and copyright headers

--- cmake/DownloadFile.cmake
+++ cmake/DownloadFile.cmake
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: © 2020 Alias Developers
+# SPDX-FileCopyrightText: © 2019 SpectreCoin Developers
+# SPDX-License-Identifier: MIT
+
 function(download_file url filename hash_type hash)
     if (NOT EXISTS ${filename})
         file(DOWNLOAD ${url} ${filename}
