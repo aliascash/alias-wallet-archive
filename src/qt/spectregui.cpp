0462b41c4e404374f48f828c3820d786f550be4a
teknex <teknex@gmail.com>
2020-09-08 08:37:13 +0200

    Android: omit qrc:/// for web resources

--- src/qt/spectregui.cpp
+++ src/qt/spectregui.cpp
@@ -118,6 +118,15 @@ void SpectreGUI::WebElement::setContent(QString value)
     spectreGUI->runJavaScript(javascriptCode);
 }
 
+QString webviewResource(QString resource)
+{
+#ifdef ANDROID
+    return resource;
+#else
+    return "qrc:///" + resource;
+#endif
+}
+
 SpectreGUI::SpectreGUI(QWebChannel *webChannel, QWidget *parent):
     QMainWindow(parent),
     bridge(new SpectreBridge(this)),
@@ -525,7 +534,7 @@ void SpectreGUI::setNumConnections(int count)
         syncingIconText.addClass("invisible");
         syncingIconText.removeClass("syncing");
         syncingIcon.removeClass("syncing");
-        syncingIcon.setAttribute("src", "qrc:///assets/svg/spinner.svg");
+        syncingIcon.setAttribute("src", webviewResource("assets/svg/spinner.svg"));
         syncingIcon.setAttribute("data-title", "Checking wallet state with network");
         syncingIcon.addClass("fa-spin");
         syncingIcon.removeClass("none");
@@ -654,7 +663,7 @@ void SpectreGUI::setNumBlocks(int count, int nTotalBlocks)
         syncingIconText.addClass("invisible");
         syncingIconText.removeClass("syncing");
         syncingIcon.removeClass("fa-spin");
-        syncingIcon.setAttribute("src", "qrc:///assets/svg/synced.svg");
+        syncingIcon.setAttribute("src", webviewResource("assets/svg/synced.svg"));
         syncingIcon.removeClass("syncing");
 
         //a js script to change the style property display to none for all outofsync elements
@@ -695,7 +704,7 @@ void SpectreGUI::setNumBlocks(int count, int nTotalBlocks)
             syncingIconText.addClass("invisible");
             syncingIconText.removeClass("syncing");
             syncingIcon.removeClass("syncing");
-            syncingIcon.setAttribute("src", "qrc:///assets/svg/spinner.svg");
+            syncingIcon.setAttribute("src", webviewResource("assets/svg/spinner.svg"));
             syncingIcon.addClass("fa-spin");
         }
 
