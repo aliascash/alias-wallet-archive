b6aee094f8a25bc6dbf6c03a2b30893804e68fbc
HLXEasy <hlxeasy@gmail.com>
2020-12-01 21:39:16 +0100

    Sign apk

--- scripts/android-build.sh
+++ scripts/android-build.sh
@@ -7,10 +7,18 @@ ownLocation="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd "$ownLocation"/../cmake-build-cmdline-android-apk/ || exit 1
 
+if [[ -z ${keystorePass} ]] ; then
+    read -r -s -p "Please enter sign keystore password: " keystorePass
+    echo
+fi
+
 /root/Qt/5.15.2/android/bin/androiddeployqt \
     --input $(pwd)/android_deployment_settings.json \
     --output $(pwd)/android-build \
     --android-platform android-29 \
     --gradle \
     --aab \
+	--jarsigner \
+	--sign /etc/ssl/certs/alias-sign-keystore.jks upload \
+	--storepass "${keystorePass}" \
     | tee ../Android-Build-$(date +%Y-%m-%d_%H%M%S).log
