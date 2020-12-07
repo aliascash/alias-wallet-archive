cbed2032c7ff698f72cc82feda026bd0de0c2ac8
teknex <teknex@gmail.com>
2020-12-07 23:52:28 +0100

    Remove duplicate register of newmnemonic.password

--- src/qt/setupwalletwizard.cpp
+++ src/qt/setupwalletwizard.cpp
@@ -215,7 +215,6 @@ NewMnemonicSettingsPage::NewMnemonicSettingsPage(QWidget *parent)
     connect(passwordVerifyEdit, SIGNAL(textChanged(QString)), this, SIGNAL(completeChanged()));
 
     registerField("newmnemonic.language", languageComboBox, "currentData", "currentIndexChanged");
-    registerField("newmnemonic.password", passwordEdit);
 
     QVBoxLayout *layout = new QVBoxLayout;
 
