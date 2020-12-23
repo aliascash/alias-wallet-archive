eaa7cdc39fc74ac39e1945195069a83abea847b2
teknex <teknex@gmail.com>
2020-12-23 19:57:59 +0100

    Add biometric note to encryption setup wizard step

--- src/qt/setupwalletwizard.cpp
+++ src/qt/setupwalletwizard.cpp
@@ -730,8 +730,8 @@ EncryptWalletPage::EncryptWalletPage(QWidget *parent)
     setTitle(tr("Wallet Encryption"));
     setSubTitle(tr("Please enter a password to encrypt the wallet.dat file."));
 
-    topLabel = new QLabel(tr("The password protects your private keys. It will be asked on wallet startup and critical operations."
-                                " Phones with biometric authentication support (eg. fingerprint reader) can set it up later for user-friendly wallet password access."));
+    topLabel = new QLabel(tr("The password protects your private keys.<br>It will be asked on wallet startup and critical operations."
+                                "<br><br>Phones with biometric authentication support (eg. fingerprint reader) can set it up later for user-friendly wallet password access."));
     topLabel->setWordWrap(true);
 
     passwordLabel = new QLabel(tr("&Wallet Password:"));
